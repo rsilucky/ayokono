5f512fd104d099a166bce08534886810802075e5
Rsi Lucky <rsilucky6@gmail.com>
2017-12-22 21:27:51 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -27,7 +27,7 @@ if(x=="3"){xxx="/2017/11/blog-marketing.html"}
 if(x=="4"){xxx="/2017/11/qualities-for-affiliate-marketing.html"}
 if(x=="5"){xxx="/2017/11/working-home-as-affiliate-marketer.html"}
 if(x=="6"){xxx="/2017/11/advertise-on-internet.html"}
-if(x=="7"){xxx="/2017/11/advertising-for-free.html"}a_to_ve[i].href="http://www.ayosarko.xyz"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="7"){xxx="/2017/11/advertising-for-free.html"}a_to_ve[i].href="https://ayosarko.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
